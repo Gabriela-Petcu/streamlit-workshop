00815808b1bb02c451ec3732048b27e079deb767
gabrielapetcu <gabriela.petcu@student.unitbv.ro>
2025-07-18 13:54:15 +0300

    second try

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
 
-st.title("streamlit workshop intro")
+st.write("Streamlit has lots of fans in the geo community. 🌍 It supports maps from PyDeck, Folium, Kepler.gl, and others.")
 
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
-
-if st.button("Send balloons!"):
-    st.balloons()
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
